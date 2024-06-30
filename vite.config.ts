9ed4b5f10cc45e06bae2f4b0382628e21c55757d
Eugene M <xexyz@yandex.ru>
2024-06-30 12:18:11 +0300

    fix wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./web-ifc.wasm",
+          dest: "./",
+        },
+        {
+          src: "./project1.ifc",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
 });
